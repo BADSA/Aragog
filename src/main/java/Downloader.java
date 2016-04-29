325b8ff2e55f3ae4ec8618da37cb589bad3d8a86
Melvin Alonso Elizondo Pérez <melvin.aep@gmail.com>
2016-04-28 21:54:49 -0600

    Basic Structure

---
+++ src/main/java/Downloader.java
@@ -0,0 +1,30 @@
+
+
+public class Downloader implements Runnable {
+
+    public Downloader() {
+
+    }
+
+
+    public void run() {
+
+    }
+
+}
+
+/*
+        DescargadorSeguro d1 = new DescargadorSeguro(1, cont);
+        DescargadorSeguro d2 = new DescargadorSeguro(2, cont);
+        DescargadorSeguro d3 = new DescargadorSeguro(3, cont);
+
+        // Pone en ejecución varios hilos
+        Thread t1 = new Thread(d1);
+        Thread t2 = new Thread(d2);
+        Thread t3 = new Thread(d3);
+
+
+        t1.start();
+        t2.start();
+        t3.start();
+ */
