05d393a6d226398f3a3053ea30348c7db29118f7
SD <superdaniel1920@hotmail.com>
2016-04-29 03:53:03 -0600

    Scheduler not finished

--- src/main/java/Scheduler.java
+++ src/main/java/Scheduler.java
@@ -1,5 +1,22 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scheduler {
+    private QueueManager queueManager;
+    private DBManager dbManager;
 
+    public Scheduler() {
+        this.dbManager = new DBManager(CONFIG.database);
+        this.queueManager = new QueueManager();
+    }
 
+    private void loadURLS() {
+        List<Pair<Integer, String>> urlsList = dbManager.getURLs();
+
+        for (Pair<Integer, String> url : urlsList) {
+           queueManager.addURL(url);
+        }
+    }
 }
